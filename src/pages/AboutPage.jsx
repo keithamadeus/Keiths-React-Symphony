6069c0cb42e267772f058844192ed98d49271bd8
Keith <keith.amadeus.williams@gmail.com>
2024-10-07 17:42:19 -0600

    update about page with profile pic, along with txt content, made change to profile pg, but adjustments are still commented out

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,59 +1,50 @@
 // This is a static page mocking an "About Us" section for our fake user data
+import profilePicture from '../assets/images/keith-trees.jpeg';
+import bannerPicture from '../assets/images/02-hero-bg.jpg';
 export default function AboutPage() {
   return (
     <div className="container pt-4">
-      <p>
-      I am a passionate and hard working Engineer and Leader. I have a thirst for learning new technology and finding creative ways to solve problems. I love building beautiful and reusable code. I thrive working with a team to achieve success together. I have excellent interpersonal communication and organizational skills. I am excited and ready to continue to learn and sharpen the knowledge I am bringing to the table, because with Software Development you never finish learning.
-      </p>
+      <h1 className="d-flex justify-content-center">About Me</h1>
+      <div className='row'>
+        <div className="col-md-2 text-center">
+        <img 
+            src={profilePicture} 
+            alt="Profile" 
+            className="rounded float-left opacity-70" 
+            style={{ width: '100px', height: '150px' }} 
+          />
+        </div>
+        <div className="col-md-9">
+          <p className="lead mb-0">
+          I am a passionate and hard working Engineer and Leader. I have a thirst for learning new technology and finding creative ways to solve problems. I love building beautiful and reusable code. I thrive working with a team to achieve success together. I have excellent interpersonal communication and organizational skills. I am excited and ready to continue to learn and sharpen the knowledge I am bringing to the table, because with Software Development you never finish learning.
+          </p>
+        </div>
+
+        
+      </div>
       <section className="features-icons bg-light text-center m-4">
         <div className="container">
           <div className="row p-2">
-            <div className="col-lg-4">
+            <div className="col-lg-12">
               <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                 <div className="features-icons-icon d-flex">
                   <i className="bi-window m-auto text-primary" />
                 </div>
-                <h3>Fully Responsive</h3>
+                <img 
+                  src={bannerPicture} 
+                  alt="Profile" 
+                  className="img-fluid rounded opacity-75" 
+                  style={{ width: '100%', height: 'auto' }} 
+                />
+                <h3>If you write the problem down clearly, then the matter is half solved</h3>
                 <p className="lead mb-0">
-                  This theme will look great on any device, no matter the size!
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-layers m-auto text-primary" />
-                </div>
-                <h3>Bootstrap 5 Ready</h3>
-                <p className="lead mb-0">
-                  Featuring the latest build of the new Bootstrap 5 framework!
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Easy to Use</h3>
-                <p className="lead mb-0">
-                  Ready to use with your own content, or customize the source
-                  files!
+                Remember, if code looks too perfect, you might just be doing it wrong. Embrace the chaos, debug with glee, and let&apos;s see what masterpiece is created
                 </p>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
-      </p>
     </div>
   );
 }
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,19 +1,48 @@
-// import card from '../components/card';
+// import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function PortfolioPage() {
-      return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card&apos;s content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      );
+  const projects = [
+    { title: 'Project 1', text: 'Description for project 1', imgSrc: 'holder.js/100px180' },
+    { title: 'Project 2', text: 'Description for project 2', imgSrc: 'holder.js/100px180' },
+    { title: 'Project 3', text: 'Description for project 3', imgSrc: 'holder.js/100px180' },
+    { title: 'Project 4', text: 'Description for project 4', imgSrc: 'holder.js/100px180' },
+    { title: 'Project 5', text: 'Description for project 5', imgSrc: 'holder.js/100px180' },
+    { title: 'Project 6', text: 'Description for project 6', imgSrc: 'holder.js/100px180' },
+];
+return (
+  <Container>
+      <Row>
+          {projects.map((project, index) => (
+              <Col key={index} sm={12} md={6} lg={4} className="mb-4">
+                  <Card style={{ width: '18rem' }}>
+                      <Card.Img variant="top" src={project.imgSrc} />
+                      <Card.Body>
+                          <Card.Title>{project.title}</Card.Title>
+                          <Card.Text>{project.text}</Card.Text>
+                          <Button variant="primary">Go somewhere</Button>
+                      </Card.Body>
+                  </Card>
+              </Col>
+          ))}
+      </Row>
+  </Container>
+);
+      // return (
+      //   <Card style={{ width: '18rem' }}>
+      //     <Card.Img variant="top" src="holder.js/100px180" />
+      //     <Card.Body>
+      //       <Card.Title>Card Title</Card.Title>
+      //       <Card.Text>
+      //         Some quick example text to build on the card title and make up the
+      //         bulk of the card&apos;s content.
+      //       </Card.Text>
+      //       <Button variant="primary">Go somewhere</Button>
+      //     </Card.Body>
+      //   </Card>
+      // );
 }
